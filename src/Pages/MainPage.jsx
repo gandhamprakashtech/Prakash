7b2a2cacd3256f69bf63acf5e41e1f16e9c9c751
gandhamprakashtech <gandhamprakashtech@gmail.com>
2025-09-28 23:17:46 +0530

    latest changes on resume

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -81,7 +81,7 @@ function Home() {
                   tools and frameworks to create optimized user experiences.
                 </p>
                 <a
-                  href="https://drive.google.com/file/d/1PsfIhJbqRgvQ3Y_Zn7brYqznncALxcMx/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1QJAcI3umUcryvssu7Ts9NAPObfAKEyv6/view?usp=sharing"
                   className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-neutral-100 hover:underline"
                 >
                   Download Resume
